d39329b1b55c9821ebad918639f2ee79d39311b3
kvitee <kvitee41@gmail.com>
2026-07-26 20:33:42 +0800

    [zshrc] Update completions module.
    Add compaudit run to check for unsafe completions;
    Speed up zsh startup by generating compdump file only if it doesn't
    exist or it is older than 24 hours.

--- core/dot-config/zsh/completions.zsh
+++ core/dot-config/zsh/completions.zsh
@@ -1,8 +1,31 @@
 # Load completion system
-autoload -Uz compinit
+autoload -Uz compinit compaudit
 
-# Initialize completion system with cached metadata
-compinit -d "$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
+# Enable advanced patterns and regular expressions
+setopt extendedglob
+
+# zcompdump file location (cache)
+local compdump_file="$XDG_CACHE_HOME/zsh/zcompdump-v$ZSH_VERSION"
+
+# Run compaudit
+local -aU insecure_dirs
+insecure_dirs=(${(f@):-"$(compaudit)"})
+
+# If audit was successful load completions
+if [[ $? -eq 0 ]]; then
+  # Load from cache if compdump file exists and generated in the last 24 hours,
+  # else (re)generate it.
+  if [[ -e "$compdump_file" && -n "$compdump_file"(#qN.mh-24) ]]; then
+    compinit -C -d "$compdump_file"
+  else
+    compinit -d "$compdump_file"
+  fi
+else
+  if [[ $#insecure_dirs -ne 0 ]]; then
+    echo "[compaudit] Insecure directories was found:"
+    ls -ld "${(@)insecure_dirs}"
+  fi
+fi
 
 # Enable interactive completion select
 zstyle ":completion:*" menu select
