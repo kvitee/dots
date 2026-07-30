4e263d8e568bac19d85df2fcafdc815735db73f5
kvitee <kvitee41@gmail.com>
2026-07-30 10:49:46 +0800

    [zshrc] Fix zsh-vi-mode plugin parsing issue.

--- core/dot-config/zsh/plugins.zsh
+++ core/dot-config/zsh/plugins.zsh
@@ -1,3 +1,6 @@
+# Enable pcre to fix some issues of zsh-vi-mode plugin
+setopt re_match_pcre
+
 # Source all plugins from `plugins` directory
 for plugin in "$ZDOTDIR/plugins"/*; do
   if [ -e "$plugin/${plugin##*/}.zsh" ]; then
