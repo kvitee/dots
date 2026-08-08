b5d24b2acc898ebbd7abb003cc83b749f4d6094f
kvitee <kvitee41@gmail.com>
2026-08-08 15:12:38 +0800

    [fzf] Move fzf config to separate file.

--- core/dot-config/zsh/fzf.zsh
+++ core/dot-config/zsh/fzf.zsh
@@ -6,6 +6,9 @@ fi
 # Source fzf completions and keybindings
 source <(fzf --zsh)
 
+# Config file
+export FZF_DEFAULT_OPTS_FILE="$XDG_CONFIG_HOME/fzf/config"
+
 # Search with `fd` if installed
 if command -v fd &> /dev/null; then
   local fd_flags="--strip-cwd-prefix --hidden --follow --exclude .git"
@@ -14,20 +17,6 @@ if command -v fd &> /dev/null; then
   export FZF_ALT_C_COMMAND="fd --type d $fd_flags"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
-
-# UI options and keybinding
-export FZF_DEFAULT_OPTS="
-  --border
-  --layout=reverse
-  --height=60%
-  --popup=bottom,100%,60%
-  --bind='ctrl-t:top'
-  --bind='ctrl-h:toggle-preview'
-  --bind='ctrl-u:preview-up'
-  --bind='ctrl-d:preview-down'
-  --bind='ctrl-U:preview-half-page-up'
-  --bind='ctrl-D:preview-half-page-down'
-"
 
 # Preview files with `bat` if installed
 if command -v bat &> /dev/null; then
