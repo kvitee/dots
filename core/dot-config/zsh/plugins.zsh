925f8c1a6f2ac7dab48598ca499a93ecb88bc517
kvitee <kvitee41@gmail.com>
2026-09-15 13:34:50 +0800

    [zsh] Add some settings for zsh-vi-mode plugin.

--- core/dot-config/zsh/plugins.zsh
+++ core/dot-config/zsh/plugins.zsh
@@ -7,3 +7,9 @@ for plugin in "$ZDOTDIR/plugins"/*; do
     source "$plugin/${plugin##*/}.zsh"
   fi
 done
+
+# Start vi-mode in Normal mode
+ZVM_LINE_INIT_MODE=$ZVM_MODE_NORMAL
+
+# Map Ctrl-r in Insert mode to open fzf history
+zvm_after_init_commands+=("zvm_bindkey viins '^R' fzf-history-widget")
